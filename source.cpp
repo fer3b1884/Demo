41b685a6f8b360ba518b29550395a30492a8a35d
Konstantin Shved <2006SKA@gmail.com>
2025-02-26 12:12:34 +0300

    Добавление сложения чисел a + b

--- source.cpp
+++ source.cpp
@@ -11,5 +11,15 @@ int main()
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "RU");
 
+    int a(0), b(0);
+
+    cout << "¬ведите число a: ";
+    cin >> a;
+
+    cout << "¬ведите число b: ";
+    cin >> b;
+
+    cout << "a + b = " << a + b << endl;
+
     return 0;
 }
