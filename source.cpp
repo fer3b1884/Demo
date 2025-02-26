477d3cef9fa05d8e607c2f69e6e324fb7e35b656
Konstantin Shved <2006SKA@gmail.com>
2025-02-26 12:10:11 +0300

    Начало работы

---
+++ source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "RU");
+
+    return 0;
+}
